21a5bc7efc7187a923438cad557cfd21a070bf31
Bernhard Wilson <wilson.protected@gmail.com>
2024-06-05 19:23:42 +0300

    update gems icons

--- src/Views/Components/Circle.php
+++ src/Views/Components/Circle.php
@@ -9,28 +9,28 @@ class Circle extends Iconable
     public function outline(): string
     {
         return <<<'blade'
-<circle cx="12" cy="12" r="11" />
+<circle cx="12" cy="12" r="9" />
 blade;
     }
 
     public function solid(): string
     {
         return <<<'blade'
-<circle cx="12" cy="12" r="11" />
+<circle cx="12" cy="12" r="10" />
 blade;
     }
 
     public function mini(): string
     {
         return <<<'blade'
-<circle cx="10" cy="10" r="9" />
+<circle cx="10" cy="10" r="8" />
 blade;
     }
 
     public function micro(): string
     {
         return <<<'blade'
-<circle cx="8" cy="8" r="7" />
+<circle cx="8" cy="8" r="6" />
 blade;
     }
 }
